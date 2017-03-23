551c91a285290a3f74c78c5531773cb7c167071e
BitSails <mrhthomp@gmail.com>
2017-03-23 11:02:20 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+#Random stuff
+
+Just a simple program done in openframeworks.
+
+
+# How to Compile
+
+This project is setup to use `make` (to do the compiling) and `make run` to run
---
+++ src/ofApp.cpp
@@ -0,0 +1,80 @@
+#include "ofApp.h"
+
+//--------------------------------------------------------------
+void ofApp::setup(){
+jewel.load("diamond.png");// image to use (must be stored in "random/bin/data" folder)
+X = {500, 1000, 30, 100, 300, 200, 750, 380 };// set of x-coordinates for jewel
+Y = {630, 400, 900,40, 200, 0, 90, 30}; //set of y-coordinates for jewel
+}
+
+//--------------------------------------------------------------
+void ofApp::update(){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::draw(){
+ofBackground(0);
+
+for(auto i = 0u; i < X.size(); i++)
+ {
+   jewel.draw(X[i], Y[i]);
+ }
+}
+
+
+
+//--------------------------------------------------------------
+void ofApp::keyPressed(int key){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::keyReleased(int key){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseMoved(int x, int y ){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseDragged(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mousePressed(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseReleased(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseEntered(int x, int y){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseExited(int x, int y){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::windowResized(int w, int h){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::gotMessage(ofMessage msg){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::dragEvent(ofDragInfo dragInfo){ 
+
+}
